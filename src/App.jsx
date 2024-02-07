48500f5c9c75a6ec4fe0377d8422671f66169d00
Prasanna6060 <ranjitkhanal61@gmail.com>
2024-02-07 11:41:28 +0545

    defining Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div >
-      
+    <div className='w-full h-screen bg-zinc-950 text-white'>
+      < Navbar />
     </div>
   )
 }
